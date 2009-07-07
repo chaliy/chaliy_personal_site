3924584d4c96eebb3d83fda0a61c721f4c02ed84
Mike Chaliy <mike@chaliy.name>
2009-07-08 00:39:25 +0300

    Fixed type

--- db/migrate/20090707210156_add_pub_date_to_entry.rb
+++ db/migrate/20090707210156_add_pub_date_to_entry.rb
@@ -1,6 +1,6 @@
 class AddPubDateToEntry < ActiveRecord::Migration
   def self.up
-    add_column :entries, :pubDate, :dateTime
+    add_column :entries, :pubDate, :datetime
   end
 
   def self.down
