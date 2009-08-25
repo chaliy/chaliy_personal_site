f41de43262bd18ba06ed0e281a68b1f0b3c44115
Mike Chaliy <mike@chaliy.name>
2009-08-25 21:58:48 +0300

    Routes now more precised

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ ActionController::Routing::Routes.draw do |map|
   map.blog_drafts 'blog/drafts', :controller => 'blog', :action => 'drafts'
   map.blog_atom 'blog.atom', :controller => 'blog', :action => 'index', :format => 'atom'
   map.blog_drafts_atom 'blog_drafts.atom', :controller => 'blog', :action => 'drafts', :format => 'atom'
-  map.blog_entry 'blog/:year/:month/:name', :controller => 'blog', :action => 'entry'
+  map.blog_entry 'blog/:year/:month/:name',
+    :controller => 'blog',
+    :action => 'entry',
+    :year => /\d{4}/,
+    :month => /\d{1,2}/  
+
    
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
