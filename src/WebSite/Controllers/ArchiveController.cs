ef2f012dc7ef2767dee1d57201eb739a1838345d
Mike Chaliy <mike@chaliy.name>
2011-04-17 00:20:37 +0300

    Added archive of the old posts

---
+++ src/WebSite/Controllers/ArchiveController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class ArchiveController : Controller
+    {
+        public ActionResult Get(string path)
+        {
+            var realPath = Server.MapPath("~/content/" + path);
+            var content = System.IO.File.ReadAllText(realPath);
+            return Content(content);
+        }
+    }
+}
